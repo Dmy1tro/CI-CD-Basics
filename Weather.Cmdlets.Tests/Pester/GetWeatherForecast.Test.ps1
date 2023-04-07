43d4e19b65d50e16c52f1347a8fa8a87e357f21f
Dmytro Laskuryk <dimonsensey@gmail.com>
2023-04-07 12:50:22 +0300

    Update test

--- Weather.Cmdlets.Tests/Pester/GetWeatherForecast.Test.ps1
+++ Weather.Cmdlets.Tests/Pester/GetWeatherForecast.Test.ps1
@@ -1,18 +1,22 @@
-if (-not (Get-Module -ListAvailable -Name Pester | Where-Object {$_.Version -eq "5.2.2"})) {
-    Install-Module -Name Pester -RequiredVersion 5.2.2 -Scope CurrentUser -Force
+Describe "Get-WeatherForecast" {
+    it "Should return weather" {
+        if (-not (Get-Module -ListAvailable -Name Pester | Where-Object {$_.Version -eq "5.2.2"})) {
+            Install-Module -Name Pester -RequiredVersion 5.2.2 -Scope CurrentUser -Force
+        }
+
+        if (-not (Get-Module -Name Pester)) {
+            Import-Module Pester -RequiredVersion 5.2.2 -Force
+        }
+
+        If (-not (Get-Module Weather.Cmdlets))
+        {
+            $pathToCmdletsProject = "$PSScriptRoot/../../Weather.Cmdlets"
+            dotnet build "$pathToCmdletsProject/Weather.Cmdlets.csproj" -c Release
+            Import-Module "$pathToCmdletsProject/bin/Release/net6.0/WeatherCmdlets.psd1"
+        }
+
+        $weather = Get-WeatherForecast
+
+        $weather | Should -Not -BeNullOrEmpty
+    }
 }
-
-if (-not (Get-Module -Name Pester)) {
-    Import-Module Pester -RequiredVersion 5.2.2 -Force
-}
-
-If (-not (Get-Module Weather.Cmdlets))
-{
-    $pathToCmdletsProject = "$PSScriptRoot/../../Weather.Cmdlets"
-    dotnet build "$pathToCmdletsProject/Weather.Cmdlets.csproj" -c Release
-    Import-Module "$pathToCmdletsProject/bin/Release/net6.0/WeatherCmdlets.psd1"
-}
-
-$weather = Get-WeatherForecast
-
-$weather | Should -Not -BeNullOrEmpty
